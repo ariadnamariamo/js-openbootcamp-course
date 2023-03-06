7455116ddd40a92688d07eb50397a5490b43c57c
Ariadna Mora <ariadnamaria9821@gmail.com>
2023-03-06 13:49:47 -0400

    feat exercise 2

---
+++ exercise 02/exercise02.js
@@ -0,0 +1,22 @@
+//Crea un nuevo archivo JS que contenga una lista con los siguientes elementos:
+//- Tu nombre (string)
+//- Tu edad (number)
+//- ¿Eres desarrollador? (boolean)
+//- Tu fecha de nacimiento (Date)
+//- Tu libro favorito (Objeto con propiedades: titulo, autor, fecha, url)
+
+const fullname = ["Ariadna Mora"];
+const age = [25];
+const developer = true;
+const birth = new Date(1998, 1, 21);
+
+const Lista = [fullname, age, developer, birth];
+
+console.log(Lista)
+
+const book = {
+        title:"¿Por qué a mí?",
+        author:"Valéria Piassa Polizzi",
+        date: new Date('1997'),
+        url:"https://www.amazon.com/-/es/Val%C3%A9ria-Piassa-Polizzi/dp/9681909208",
+}
